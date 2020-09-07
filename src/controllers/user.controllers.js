050c142025c6f7e8e442ef2e90118a12941314c0
Robotic7o7 <scientificrohan@gmail.com>
2020-09-07 14:34:25 +0530

    Fixed bugs

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,6 +1,5 @@
 const User = require('../models/user.model.js');
 const nodemailer = require('nodemailer');
-const out = require('./MailTemplate.js');
 
 // Retrieve and return all users from the database.
 exports.findAll = (req, res) => {
@@ -39,15 +38,79 @@ exports.create = (req, res) => {
 });
 
 //HTML Template 
+var output = `<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Confirmation Mail</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+            <center class="m_-8901952281184217417wrapper">
+                    <div>
+                      <table cellpadding="0" cellspacing="0" border="0" width="100%" class="m_-8901952281184217417wrapper" bgcolor="#f5f5f5">
+                        <tbody><tr>
+                          <td valign="top" bgcolor="#f5f5f5" width="100%">
+                            <table width="100%" role="content-container" align="center" cellpadding="0" cellspacing="0" border="0">
+                              <tbody><tr>
+                                <td width="100%">
+                                  <table width="100%" cellpadding="0" cellspacing="0" border="0">
+                                    <tbody><tr>
+                                      <td>
+                                        
+                                        <table width="100%" cellpadding="0" cellspacing="0" border="0" style="width:100%;max-width:600px" align="center">
+                                          <tbody><tr>
+                                            <td role="modules-container" style="padding:0px 0px 0px 0px;color:#000000;text-align:left" bgcolor="#ffffff" width="100%" align="left">
+                                              
+                                                <table class="m_-8901952281184217417preheader" role="module" border="0" cellpadding="0" cellspacing="0" width="100%" style="display:none!important;opacity:0;color:transparent;height:0;width:0">
+                                         <tbody><tr>
+                                            <td role="module-content">
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                
+                            <table role="module" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout:fixed">
+                    <tbody><tr>
+                      <td style="padding:0px 0px 20px 0px" role="module-content" bgcolor="#F5F5F5">
+                      </td>
+                    </tr>
+                  </tbody></table>
+                  <table class="m_-8901952281184217417wrapper" role="module" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout:fixed">
+                        <tbody><tr>
+                          <td style="font-size:6px;line-height:10px;padding:0px 0px 0px 0px" valign="top" align="center">
+                            <img class="m_-8901952281184217417max-width CToWUd a6T" border="0" style="display:block;color:#000000;text-decoration:none;font-family:Helvetica,arial,sans-serif;font-size:16px;max-width:100%!important;width:100%;height:auto!important" src="https://lh3.googleusercontent.com/KAj2033_4fi0SHx9UP7gJPgOtZZPCBKv78_We-BqVOZXiTiEPcbssOQ3Ym5TkJtn2PJ1tok81G90Z5H4vroTYr9lT7JOD9ckfykJwkcDCuc_hSC0JPkXp37-13cw7Avh8HOzX7tqAQ=w2400" alt="Registration Successful" width="600" tabindex="0">
+                          </td>
+                        </tr>
+                      </tbody></table>
+                  <table role="module" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout:fixed">
+                        <tbody><tr>
+                          <td style="padding:18px 18px 18px 18px;line-height:22px;text-align:inherit;background-color:#b0cae9" height="100%" valign="top" bgcolor="#ffffff">
+                
 
-
-//FirstHalf of the Template
-
-var temp1 = out.template
-
-//SecondHalf of the Template
-
-var temp2 = `<div><ul>
+                  <table class="m_-8901952281184217417wrapper" role="module" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout:fixed">
+                        <tbody><tr>
+                          <td style="font-size:6px;line-height:10px;padding:0px 0px 0px 0px" valign="top" align="center">
+                            <img class="m_-8901952281184217417max-width CToWUd a6T" border="0" style="display:block;color:#000000;text-decoration:none;font-family:Helvetica,arial,sans-serif;font-size:16px;max-width:100%!important;width:100%;height:auto!important" src="https://lh3.googleusercontent.com/fzBiczsR7krJuMoKftaK-5-3g9csn7AYxBwida3zv_Ia9MgQHB2GdRNU_k6tOzw-ZVsdWEeZo6BUb-30fAUxuLQbpQagVveFwHtlcDiSOdc0HTErik3BrPtl-X7lq_u9daOMRsfbtA=w2400" alt="Registration Successful" width="600" tabindex="0">
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                
+                  <table role="module" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout:fixed">
+                    <tbody><tr>
+                      <td style="padding:18px 18px 18px 18px;line-height:22px;text-align:inherit;background-color:#ffffff" height="100%" valign="top" bgcolor="#ffffff">
+                          
+              
+              
+              <div>
+              <div dir="ltr">
+              <div><p align="center"><strong><em>"The capacity to learn is a gift; The ability to learn is a skill; The willingness to learn is a choice."</em></strong></p></div>
+              <div><p align="center">We have received your application for an invite to the event <strong>Event Name</strong>. Your response has been recorded. Thank you for showing your interest.</div>
+              <div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>This is the information that we got: </strong></div>
+              <div><ul>
                   <li>First Name: ${req.body.fname}</li>
                   <li>Last Name: ${req.body.lname}</li>
                   <li>Email ID: ${req.body.email}</li>
@@ -67,12 +130,11 @@ var temp2 = `<div><ul>
     </body>
 </html>` 
 
-var output= temp1+temp2;
 
 var mailOptions = {
 	from: 'Rohan Christopher <rohan.stutalk@gmail.com>',
 	to: `${req.body.email}`,
-	subject:'Test NodeMailer',
+	subject:'Registration Successful - Event Name',
 	html:`${output}`
 };
 
